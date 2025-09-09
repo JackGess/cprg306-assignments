fbc68705c293343cbb252aa88fa7597c04abe8c9
Jack Gess <jgess44@gmail.com>
2025-09-09 14:10:52 -0600

    Finished Week-2 Assignment
    Adds week-2 folder with page and student info

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo(){
+
+
+    return(
+        <div>
+            <h3>Jack Gess</h3>
+            <Link href="https://github.com/JackGess/cprg306-assignments">My GitHub Repository</Link>
+        </div>
+    );
+}
